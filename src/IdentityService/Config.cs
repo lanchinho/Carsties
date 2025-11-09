7dc51fec012c959f33a055680d73b79a32f1ae43
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2025-11-08 22:41:10 -0300

    fix: sorting behaviour

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -39,7 +39,8 @@ public static class Config
 				RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
 				AllowOfflineAccess = true,
 				AllowedScopes = {"openid", "profile", "auctionApp"},
-				AccessTokenLifetime = 3600
+				AccessTokenLifetime = 3600,
+				AlwaysIncludeUserClaimsInIdToken = true
 			}
 		};
 }
