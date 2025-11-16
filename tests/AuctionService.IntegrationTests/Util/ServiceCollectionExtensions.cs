04663f6f16d64110087e3a47955ff92a940aace5
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2025-11-16 15:36:43 -0300

    adding integration tests.

---
+++ tests/AuctionService.IntegrationTests/Util/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionService.IntegrationTests.Util;
+internal static class ServiceCollectionExtensions
+{
+	internal static void RemoveDbContext<T>(this IServiceCollection services)
+	{
+		var descriptor = services.SingleOrDefault(d =>
+			d.ServiceType == typeof(DbContextOptions<AuctionDbContext>));
+
+		if (descriptor != null) services.Remove(descriptor);
+	}
+
+
+	internal static void EnsureCreated<T>(this IServiceCollection services)
+	{
+		var sp = services.BuildServiceProvider();
+		using var scope = sp.CreateScope();
+		var scoppedServices = scope.ServiceProvider;
+		var db = scoppedServices.GetRequiredService<AuctionDbContext>();
+
+		db.Database.Migrate();
+		DbHelper.InitDbForTests(db);
+	}
+}
