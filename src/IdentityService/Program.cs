3c50802ae3de6669a4a2c2c1f9a4ef9fdfe14e7e
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2025-11-16 18:21:47 -0300

    Preparing the application for k8s

--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -2,6 +2,8 @@ using System.Globalization;
 using System.Text;
 using Duende.IdentityServer.Licensing;
 using IdentityService;
+using Npgsql;
+using Polly;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -19,9 +21,11 @@ try
 		.ConfigureServices()
 		.ConfigurePipeline();
 
-	// this seeding is only for the template to bootstrap the DB and users.
-	// in production you will likely want a different approach.
-	SeedData.EnsureSeedData(app);
+	var retryPolicy = Policy
+		.Handle<NpgsqlException>()
+		.WaitAndRetry(5, retryAtempt => TimeSpan.FromSeconds(5));
+	
+	retryPolicy.ExecuteAndCapture(() => SeedData.EnsureSeedData(app));
 
 	if (app.Environment.IsDevelopment())
 	{
